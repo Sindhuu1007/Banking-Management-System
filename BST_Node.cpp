ec6d4c345588a6abb0d6791404ed027840af7928
Sindhuu1007 <cs23b026@smail.iitm.ac.in>
2025-06-12 15:30:08 +0530

    initial commit

--- BST_Node.cpp
+++ BST_Node.cpp
@@ -1,18 +1,16 @@
 
 # include "BST_Node.h"
 
-BST_Node:: BST_Node()
-{
+BST_Node:: BST_Node(){
 	left = nullptr;
 	right = nullptr;
 	name = "";
-    adress = "";
-    account_number = 0;
-    password = 0;
-    balance = 0;
+	adress = "";
+	account_number = 0;
+	password = 0;
+	balance = 0;
 }
-BST_Node:: BST_Node(string name, string adress, int accountno, int password, int balance)
-{
+BST_Node:: BST_Node(string name, string adress, int accountno, int password, int balance){
 	left = nullptr;
 	right = nullptr;
 	this->name = name;
