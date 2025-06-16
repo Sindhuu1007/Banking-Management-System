ce6d050b2d87e889c85701784d7168a370dafeb1
Sindhuu1007 <cs23b026@smail.iitm.ac.in>
2025-06-16 11:18:39 +0530

    Updated

--- main.cpp
+++ main.cpp
@@ -6,44 +6,33 @@
 # include "customer.h"
 # include "string.h"
 
-void boot()
-{
+void boot(){
 	Hashtable H;
 	BST_Tree T;
 	H.starthash();
 	T.load_Server();
 }
-int main()
-{
+int main(){
 	void boot();
-
 	int condition=0;
-	while (condition != 4)
-	{
+	while (condition != 4){
 		cout << "YOU want to login as:\t\t\t\t\t\t" << endl << endl;
 		cout << "1-ADMIN" << endl;
 		cout << "2-STAFF" << endl;
 		cout << "3-CUSTOMER" << endl;
 		cin >> condition;
-		if (condition == 1)
-		{
+		if (condition == 1){
 			admin();
 		}
-		if (condition == 2)
-		{
-
-		}
-		if (condition == 3)
-		{
+		if (condition == 2){}
+		if (condition == 3){
 			customer();
 		}
-		if (condition == 4)
-		{
+		if (condition == 4){
 			condition = 4;
 		}
 	}
 
-	
 	system("pause");
 	return 0;
 }
