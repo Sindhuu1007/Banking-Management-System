54c6cbdd806addfad8e4e2baaa4a5029d857ece4
Sindhuu1007 <cs23b026@smail.iitm.ac.in>
2025-06-16 11:22:43 +0530

    Updated

--- Node_1.h
+++ Node_1.h
@@ -1,11 +1,13 @@
-#pragma once
-#pragma once
-class Node_1
-{
+#ifndef NODE_1_H
+#define NODE_1_H
+
+class Node_1{
 public:
-	Node_1 * next;
+	Node_1* next;
 	int accountNumber;
 	int password;
 	Node_1();
 	Node_1(int, int);
 };
+
+#endif
