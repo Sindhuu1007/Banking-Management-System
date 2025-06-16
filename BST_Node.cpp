177199c4a78e31d85e0d527e0d20804fbe14a8b0
Sindhuu1007 <cs23b026@smail.iitm.ac.in>
2025-06-16 11:30:23 +0530

    Update BST_Node.cpp

--- BST_Node.cpp
+++ BST_Node.cpp
@@ -1,6 +1,22 @@
+#ifndef BST_NODE_H
+#define BST_NODE_H
+# include <bits/stdc++.h>
+using namespace std;
 
-# include "BST_Node.h"
+class BST_Node {
+public:
+	BST_Node * left;
+	BST_Node * right;
+	string name;
+	string adress;
+	int account_number;
+	int password;
+	int balance;
 
+	BST_Node();
+	BST_Node(string, string, int, int, int);
+	
+};
 BST_Node:: BST_Node(){
 	left = nullptr;
 	right = nullptr;
